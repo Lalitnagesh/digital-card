a1f96ddcad72ef1b64c9698f634cad7fffcf946e
lalitnagesh <lalitnagesh143@gmail.com>
2024-07-19 00:52:52 +0530

    fixed

--- public/admin/js/custom.js
+++ public/admin/js/custom.js
@@ -1,23 +1,24 @@
-$(document).ready(function(){
+$(document).ready(function () {
     // Check Admin password correct or not
-    $('#current_password').keyup(function(){
-        var curretnt_password  = $('#current_password').val();
+    $('#current_password').keyup(function () {
+        var current_password = $('#current_password').val();
 
         $.ajax({
             headers: {
                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             },
             type: 'post',
-            url: '/admin/check-current-pasword',
-            data: { curretnt_password:curretnt_password },
-            success:function(response){
-                if(response == "false"){
-                    $('#verifiedPassword').html('Corrent Password is Incorrect')
-                }else if(response == "true"){
-                    $('#verifiedPassword').val('Corrent Password is Correct')
+            url: '{{ route("admin.checkCurrentPassword") }}',
+            data: { current_password: current_password },
+            success: function (response) {
+                if (response == "false") {
+                    $('#verifiedPassword').html('Current Password is Incorrect');
+                } else if (response == "true") {
+                    $('#verifiedPassword').html('Current Password is Correct');
                 }
-            },error:function () {
-                alert('Error');
+            },
+            error: function () {
+                $('#verifiedPassword').html('Current Password is Incorrect');
             }
         });
     });
